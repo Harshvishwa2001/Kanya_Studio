b1c0d681402846afcfcf1bccd124fa9f1d4b69a5
harshruntime <harshvishwa.runtime@gmail.com>
2026-01-22 16:23:11 +0530

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,16 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'kanyastudios.com', // Add this for your API images
+        hostname: 'kanyastudios.com',
         pathname: '/**',
       },
     ],
+  },
+  // This section solves the 413 "Payload Too Large" error
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '150mb', // Adjust this (e.g., 100mb) based on your video sizes
+    },
   },
 };
 
